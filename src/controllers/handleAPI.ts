8b26c16f92aaffb13ff8c357040b624b992f7db8
Husameldeen <mhusam87@gmail.com>
2026-04-01 10:19:03 +0200

    all

--- src/controllers/handleAPI.ts
+++ src/controllers/handleAPI.ts
@@ -6,7 +6,7 @@ const handleAPI = async (req: any, res: any) => {
   const { city } = req.query;
 
   const response = await axios.get(
-    `https://api.weatherapi.com/v1/forecast.json?Key=${env.WEATHER_API_KEY}&q=${city}&days=3`,
+    `https://api.weatherapi.com/v1/forecast.json?Key=${process.env.WEATHER_API_KEY}&q=${city}&days=3`,
   );
   res.json(normalizeWeather(response.data));
 };
