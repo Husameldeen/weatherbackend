2c3b35d18ad420cd95bbeb7a16c7a0a61c05b105
Husameldeen <mhusam87@gmail.com>
2026-04-01 15:15:06 +0200

    all

--- src/controllers/handleAPI.ts
+++ src/controllers/handleAPI.ts
@@ -5,10 +5,21 @@ import normalizeWeather from "./handleWeatherData.js";
 const handleAPI = async (req: any, res: any) => {
   const { city } = req.query;
 
-  const response = await axios.get(
-    `https://api.weatherapi.com/v1/forecast.json?Key=${process.env.WEATHER_API_KEY}&q=${city}&days=3`,
-  );
-  res.json(response);
+  if (!city) res.send("please enter your city!");
+
+  try {
+    const response = await axios.get(
+      `https://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${city}`,
+    );
+
+    res.json(response.data);
+    // console.log(data);
+  } catch (err) {
+    res.json(err);
+  }
 };
 
 export default handleAPI;
+
+//https://api.weatherapi.com/v1/forecast.json?Key=3329f26b8f8d4dc1829101914250412&q=london&days=3
+//https://api.weatherapi.com/v1/current.json?key=3329f26b8f8d4dc1829101914250412&q=London&aqi=no
