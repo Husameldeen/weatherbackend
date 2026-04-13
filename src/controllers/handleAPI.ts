6f98b988465d0cc61ea692d4b7fc41490d8942d1
Husameldeen <mhusam87@gmail.com>
2026-04-13 07:37:32 +0200

    issues fixed

--- src/controllers/handleAPI.ts
+++ src/controllers/handleAPI.ts
@@ -4,7 +4,6 @@ import normalizeWeather from "./handleWeatherData.js";
 
 const handleAPI = async (req: any, res: any) => {
   const { city } = req.query;
-  console.log(process.env.WEATHER_API_KEY);
 
   if (!city) res.send("please enter your city!");
 
